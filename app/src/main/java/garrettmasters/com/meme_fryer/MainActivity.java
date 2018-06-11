b69ce8b79a55a9fcb443b2a2a699beb81295bb66
Andrew Lauer <andrewlauer1@gmail.com>
2018-06-11 15:06:44 -0500

    Added listener for Meme Frying

--- app/src/main/java/garrettmasters/com/meme_fryer/MainActivity.java
+++ app/src/main/java/garrettmasters/com/meme_fryer/MainActivity.java
@@ -2,6 +2,12 @@ package garrettmasters.com.meme_fryer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +17,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openFrierActivty(View view){
+        Intent openFrierActivity = new Intent(this, FrierActivity.class);
+        startActivity(openFrierActivity);
+    }
+
+    public void loadImage(View view) {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,
+                "Select Picture"), LOAD_IMAGE);
     }
 }
