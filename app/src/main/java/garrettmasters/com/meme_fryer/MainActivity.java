3f658b5f6741e2e2a31b100c15c8a4973d3e568c
Garrett Masters <GarrettTheMasters@gmail.com>
2018-06-13 18:14:48 -0500

    Fix onClickListener situation

--- app/src/main/java/garrettmasters/com/meme_fryer/MainActivity.java
+++ app/src/main/java/garrettmasters/com/meme_fryer/MainActivity.java
@@ -4,20 +4,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int LOAD_IMAGE = 0;
 
+    private Button openFryerButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        openFryerButton = (Button) findViewById(R.id.cvButtonMain);
+        openFryerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFryerActivity();
+            }
+        });
     }
 
-    public void openFryerActivty(View view){
-        Intent openFryerActivity = new Intent(this, FryerActivity.class);
-        startActivity(openFryerActivity);
+    public void openFryerActivity() {
+        Intent intent = new Intent(this, FryerActivity.class);
+        startActivity(intent);
     }
 
     public void loadImage(View view) {
