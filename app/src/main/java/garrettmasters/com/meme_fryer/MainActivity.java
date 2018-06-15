7b585ec5fe6ea6286e736e7a51e392d8e1158bac
Garrett Masters <GarrettTheMasters@gmail.com>
2018-06-14 22:27:59 -0500

    Clean up code, comment changes

--- app/src/main/java/garrettmasters/com/meme_fryer/MainActivity.java
+++ app/src/main/java/garrettmasters/com/meme_fryer/MainActivity.java
@@ -4,34 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int LOAD_IMAGE = 0;
-    //private Button openFryerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //OnClickListener for cvButtonMain
-        //openFryerButton = (Button) findViewById(R.id.cvButtonMain);
-        //openFryerButton.setOnClickListener(new View.OnClickListener() {
-            //@Override
-            //public void onClick(View v) {
-                //openFryerActivity();
-            //}
-        //});
     }
 
     //Open FryerActivity
-    //public void openFryerActivity() {
-        //Intent intent = new Intent(this, FryerActivity.class);
-        //startActivity(intent);
-    //}
-
     public void openFryerActivity(View view) {
         Intent openFryerActivity = new Intent(this, FryerActivity.class);
         startActivity(openFryerActivity);
